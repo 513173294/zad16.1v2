890980cfb18dd20219effc92a371ad4b1d1904dc
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-07-20 17:02:25 +0200

    chyba juz jest ok

--- src/main/java/com/example/demo/StacjaPogodowa.java
+++ src/main/java/com/example/demo/StacjaPogodowa.java
@@ -14,38 +14,35 @@ public class StacjaPogodowa {
     public int getTemp() {
         int i = 0;
         if (temp == null) {
-            System.out.println("Pobieram informacje pogodowe");
-            i++;
-        }
-        if (i != 0 && i < 2) {
             int losowa = getLosowa();
-            temp=losowa;
+            System.out.println("Pobieram informacje pogodowe");
+            temp = losowa;
+
             if (temp > 10) {
                 System.out.println("1");
-                  info = "slonecznie";
+                info = "slonecznie";
             } else {
                 System.out.println("2");
                 info = "pochmurno";
             }
-            return
-                    temp ; //=losowa;
+
         }
         return temp;
-    }
 
-    private int getLosowa() {
-        Random random = new Random();
-        return temp=random.nextInt(60) - 25;
     }
+        private int getLosowa () {
+            Random random = new Random();
+            return temp = random.nextInt(60) - 25;
+        }
 
-    public String getInfo() {
-        return info;
-    }
-    //    public String getInfo() {
+        public String getInfo () {
+            return info;
+        }
+        //    public String getInfo() {
 //
 //        if (this.temp > 15) {
 //            return info = "slonecznie";
 //        }
 //        return info = "pochmurno";
 //    }
-}
+    }
